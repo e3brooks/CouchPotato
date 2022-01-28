b1702c641b33d1952c6b74168e40eab3a7f20a13
e3brooks <53798202+e3brooks@users.noreply.github.com>
2022-01-27 17:33:00 -0800

    Initial Commit

---
+++ CouchPotato/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CouchPotato
+//
+//  Created by Elisa Brooks on 1/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
